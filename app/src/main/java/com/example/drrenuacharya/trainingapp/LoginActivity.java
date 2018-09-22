5c2ec5d8b872e200a9b81ebefa345435c436c971
Sandeep Kumar <sksandeep4156@gmail.com>
2018-09-22 10:20:45 +0200

    First Commit

---
+++ app/src/main/java/com/example/drrenuacharya/trainingapp/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.drrenuacharya.trainingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//checking version control. Its working
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
